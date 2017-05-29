c6e9c15095c09441405637b504bf146280c61f29
USACO <usaco23@koala5.cs.clemson.edu>
2017-05-29 16:59:46 -0400

    make run3 cow=?

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ xd: xd.cpp
 	g++ -std=c++1y -O2 -o xd xd.cpp
 run: xd
 	./xd | ./xd | ./imview
+run3: xd
+	python run3.py cow$(cow)
---
+++ run3.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from threading import Thread
+
+cow = sys.argv[1]
+
+def run(filename):
+	os.system('make run < {}'.format(filename))
+
+Thread(target=run, args=('{}.20000.in'.format(cow),)).start()
+Thread(target=run, args=('{}.2000.in'.format(cow),)).start()
+Thread(target=run, args=('{}.200.in'.format(cow),)).start()
