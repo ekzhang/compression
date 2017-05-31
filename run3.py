0721775150bbb8f04a8d3efbf93199535f2dd4e1
Eric K. Zhang <ekzhang1@gmail.com>
2017-05-31 14:28:52 -0400

    update run3 to show original image

--- run3.py
+++ run3.py
@@ -7,6 +7,7 @@ cow = sys.argv[1]
 def run(filename):
 	os.system('make run < {}'.format(filename))
 
+Thread(target=os.system, args=('./imview < {}.200.in'.format(cow),)).start()
 Thread(target=run, args=('{}.20000.in'.format(cow),)).start()
 Thread(target=run, args=('{}.2000.in'.format(cow),)).start()
 Thread(target=run, args=('{}.200.in'.format(cow),)).start()
